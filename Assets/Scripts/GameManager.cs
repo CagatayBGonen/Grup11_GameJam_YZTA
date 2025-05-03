ef90d3a19e551034a299dc89447901266370c43d
Çağatay Bayram GÖNEN <cagataybayramgonen@gmail.com>
2025-05-03 08:29:59 +0300

    feat: Added character energy collect

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private int energyCount = 0; // enerji sayýsýný tutacak
+    public int energyGoal = 10; // toplam enerji limiti
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void CollectEnergy() // enerji sayýsýný artýrýr
+    {
+        energyCount++;
+        Debug.Log("Enerji Toplandý: " + energyCount);
+
+        if(energyCount >= energyGoal) // amaçlanan enerji seviyesine gelinip gelinmedigi kontrol ediliyior.
+        {
+            LevelCompleted(); // level tamamlandýgýnda calýstýrýlan method
+        } 
+    }
+    
+    private void LevelCompleted() // level tamamlandýgýnda calýstýrýlan metho
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // bir sonraki index sýrasýndaki scene acýlýr.
+    }
+}
