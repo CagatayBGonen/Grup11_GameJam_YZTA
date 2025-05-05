d3df826e389272999ee68c55ad7f9e437630c279
Ali Berdan Karasoy <aliberdankrsy1@gmail.com>
2025-05-05 21:33:31 +0300

    feat: last version, is done

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -3,9 +3,22 @@ using TMPro;
 using UnityEngine.SceneManagement;
 public class GameOver : MonoBehaviour
 {
-    
+    public AudioClip hoverSound; // Ses dosyasını buraya at
+    private AudioSource audioSource;
     public TextMeshProUGUI energyCountText;
 
+void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    public void PlayHoverSound()
+    {
+        if (hoverSound != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(hoverSound);
+        }
+    }
     public void Setup(int energyCount)
     {
         gameObject.SetActive(true);
